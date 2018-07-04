1c2d0f172b7d3b725d9de5c4bc452f997679255f
Danny Sat <joedukk@Dannys-Air.fios-router.home>
2018-07-04 17:08:20 -0400

    Updated formatting for numbers

--- api/controllers/minWageController.js
+++ api/controllers/minWageController.js
@@ -5,6 +5,12 @@ var cheerio = require('cheerio');
 
 var errors = [];
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 var parseLastIncrease = function (element, state, locality) {
     var lastIncreasePattern1 = /(.*)( to)(.*)(, effective )(.*)/;
     var lastIncreasePattern2 = /(.*)(, effective )(.*)/;
@@ -16,14 +22,14 @@ var parseLastIncrease = function (element, state, locality) {
         var lastIncreaseMatches2 = lastIncreaseRaw.match(lastIncreasePattern2);
         if (lastIncreaseMatches1) {
             lastIncrease = {
-                from: lastIncreaseMatches1[1],
-                to: lastIncreaseMatches1[3],
-                effective: lastIncreaseMatches1[5],
+                from: +(lastIncreaseMatches1[1].replace('$', '')),
+                to: +(lastIncreaseMatches1[3].replace('$', '')),
+                effective: new Date(lastIncreaseMatches1[5]),
             }
         } else if (lastIncreaseMatches2) {
             lastIncrease = {
-                to: lastIncreaseMatches2[1],
-                effective: lastIncreaseMatches2[3],
+                to: +(lastIncreaseMatches2[1].replace('$', '')),
+                effective: new Date(lastIncreaseMatches2[3]),
             }
         } else {
             var error = `Could not parse upcoming increase for ${state} / ${locality}. '${lastIncreaseRaw}'`;
@@ -44,7 +50,7 @@ var parseUpcomingIncrease = function (element, state, locality, note) {
     var upcomingIncreasesRaw = element.children('td:nth-child(5)').text();
     if (upcomingIncreasesRaw) {
         var upcomingIncreasesArr = upcomingIncreasesRaw.split('/');
-        
+
         var lastIncrease = upcomingIncreasesArr[upcomingIncreasesArr.length - 1];
         var hasUpcomingIncreaseIndex = false;
         if (lastIncrease.indexOf('indexing') !== -1) {
@@ -63,14 +69,14 @@ var parseUpcomingIncrease = function (element, state, locality, note) {
             // need to check for annual indexing footnote
             if (upcomingIncreaseMatches1) {
                 upcomingIncreases.push({
-                    to: upcomingIncreaseMatches1[1],
-                    effective: upcomingIncreaseMatches1[2].trim().replace('*', ''),
+                    to: +(upcomingIncreaseMatches1[1].replace('$', '')),
+                    effective: new Date(upcomingIncreaseMatches1[2].trim().replace('*', '')),
                     note: upcomingIncreaseMatches1[2].indexOf('*') !== -1 ? note.replace('*', '').trim() : null,
                 });
             } else if (upcomingIncreaseMatches2) {
                 upcomingIncreases.push({
-                    to: upcomingIncreaseMatches2[1],
-                    effective: upcomingIncreaseMatches2[2].trim().replace('*', ''),
+                    to: +(upcomingIncreaseMatches2[1].replace('$', '')),
+                    effective: new Date(upcomingIncreaseMatches2[2].trim().replace('*', '')),
                     note: upcomingIncreaseMatches2[2].indexOf('*') !== -1 ? note.replace('*', '').trim() : null,
                 });
             } else {
@@ -87,7 +93,18 @@ var parseUpcomingIncrease = function (element, state, locality, note) {
     };
 }
 
-exports.list_all_minWages = function (req, res) {
+var getCountyMappings = async () => {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM epi_county_mapping');
+
+    return result.rows;
+}
+
+exports.list_all_minWages = async (req, res) => {
+    console.log(req.query.mapCounties);
+
+    const countyMappings = await getCountyMappings();
+
     request('https://www.epi.org/minimum-wage-tracker/#/min_wage/', function (error, response, html) {
         // First we'll check to make sure no errors occurred when making the request
         if (!error) {
@@ -101,23 +118,44 @@ exports.list_all_minWages = function (req, res) {
                 try {
                     var state = $(element).find('th').text().trim();
                     var locality = $(element).children('td:nth-child(2)').text().trim() || null;
-                    var minWage = $(element).children('td:nth-child(3)').text() || federalMinWage;
+                    var minWage = +$(element).children('td:nth-child(3)').text() || federalMinWage;
                     var lastIncrease = parseLastIncrease($(element), state, locality);
                     var note = $(element).children('td:nth-child(8)').text();
                     var upcomingIncrease = parseUpcomingIncrease($(element), state, locality, note);
                     var indexing = $(element).children('td:nth-child(6)').text();
                     var lastChange = $(element).children('td:nth-child(7)').text();
 
-                    dataTable.push({
-                        state,
-                        locality,
-                        minWage,
-                        lastIncrease,
-                        upcomingIncrease: !upcomingIncrease.upcomingIncreases || upcomingIncrease.upcomingIncreases.length === 0 ? null : upcomingIncrease.upcomingIncreases,
-                        // upcomingIncreasesIndex: upcomingIncrease.upcomingIncreasesIndex.trim(),
-                        indexing,
-                        lastChange,
-                    });
+                    const data = [];
+
+                    // map epi county mappings
+                    const mappings = countyMappings.filter(countyMapping => countyMapping.state === state && countyMapping.locality === locality);
+                    if (req.query.mapCounties && req.query.mapCounties.toLowerCase() === 'true' && mappings.length > 0) {
+                        mappings.forEach(mapping => {
+                            data.push({
+                                state,
+                                locality: mapping.alias,
+                                minWage,
+                                lastIncrease,
+                                upcomingIncrease: !upcomingIncrease.upcomingIncreases || upcomingIncrease.upcomingIncreases.length === 0 ? null : upcomingIncrease.upcomingIncreases,
+                                // upcomingIncreasesIndex: upcomingIncrease.upcomingIncreasesIndex.trim(),
+                                indexing,
+                                lastChange,
+                            })
+                        })
+                    } else {
+                        data.push({
+                            state,
+                            locality,
+                            minWage,
+                            lastIncrease,
+                            upcomingIncrease: !upcomingIncrease.upcomingIncreases || upcomingIncrease.upcomingIncreases.length === 0 ? null : upcomingIncrease.upcomingIncreases,
+                            // upcomingIncreasesIndex: upcomingIncrease.upcomingIncreasesIndex.trim(),
+                            indexing,
+                            lastChange,
+                        });
+                    }
+
+                    dataTable.push(...data);
                 } catch (err) {
                     console.error(err.message);
                     errors.push(`Error while processing ${(state + ' ' || '')}${(locality + ' ' || '')}${err.message}`);
